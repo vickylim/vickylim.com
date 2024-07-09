752479629d4c70fed7823b3a869d56be065f204d
Vicky Lim <vicky.txt@gmail.com>
2024-07-09 16:17:06 -0500

    Restoring public folder with script

--- publish_site.sh
+++ publish_site.sh
@@ -5,13 +5,3 @@ cd /Users/vickylim/Documents/GitHub/vickylim.com
 
 # Open Terminal and run Hugo
 osascript -e 'tell application "Terminal" to do script "hugo"'
-
-#!/bin/bash
-
-#!/bin/bash
-
-# Navigate to your Hugo project directory
-cd /Users/vickylim/Documents/GitHub/vickylim.com
-
-# Open Terminal and run Hugo
-osascript -e 'tell application "Terminal" to do script "cd /Users/vickylim/Documents/GitHub/vickylim.com; hugo"'
